2c6b5fdf32fd89ded1f582e6befef3f17eba6e2a
wjtay1998 <wjtay1998@gmail.com>
2021-03-20 15:49:58 +0800

    Update recommendAlgo.dart
    Added actual implementation for use by app

--- lib/src/algo/recommendAlgo.dart
+++ lib/src/algo/recommendAlgo.dart
@@ -1,4 +1,5 @@
 import 'package:Procery/src/models/Ingredient.dart';
+import 'package:Procery/src/models/Purchase.dart';
 import 'package:Procery/src/models/Recipe.dart';
 import 'package:Procery/src/models/GroceryList.dart';
 import 'package:Procery/src/models/Inventory.dart';
@@ -8,12 +9,9 @@ import 'package:Procery/src/models/PlannerRecord.dart';
   TODO: adding a plannerRecord adds the groceries to the fixed grocery list
   From groceryListList -> Get active & included lists
   Calculate the no. of ingredients going to be bought
-  How about ingredients bought already,
-  cant tell if they are to be included
-  maybe just assume that they are all to be included in calc?
-  (provide option to set? current design doesnt allow this)
 
-  For each ingredient % the ingredients's fixed qty
+
+  For each ingredient ingrediented fixed q - (needed % the ingredients's fixed qty)
   --> should return the remainder
   Set the remainder as the constraints for the algo
 
@@ -24,10 +22,166 @@ import 'package:Procery/src/models/PlannerRecord.dart';
  */
 
 
-List<Recipe> getRecommend(List<GroceryList> groceryListList,
+getRecommend(List<GroceryList> groceryListList,
     List<Inventory> inventoryList, List<Ingredient> ingredientList,
     List<Recipe> recipeList){
 
+    int flourA;
+    int eggA;
+    int butterA;
+    int chocoA;
+    int milkA;
+
+    List<GroceryList> activeGL = groceryListList.where((gl) => gl.active == 1).toList();
+    for(int i = 0; i < activeGL.length; i++){
+      for(int j = 0; j < activeGL[i].purchases.length; j++){
+        Purchase curr = activeGL[i].purchases[j];
+        int toBuy = activeGL[i].purchases[j].quantity - activeGL[i].purchases[j].purchased;
+        switch(curr.ingredient.name){
+          case "Flour":
+            flourA += toBuy;
+            continue;
+          case "Egg":
+            eggA += toBuy;
+            continue;
+          case "Butter":
+            butterA += toBuy;
+            continue;
+          case "Chocolate":
+            chocoA += toBuy;
+            continue;
+          case "Milk":
+            milkA += toBuy;
+            continue;
+        }
+      }
+    }
+
+    print(" flourA: " + flourA.toString() +
+        " eggA: " + eggA.toString() +
+        " butterA: " + butterA.toString() +
+        " chocoA: " + chocoA.toString() +
+        " milkA: " + milkA.toString());
+
+    int flourC = ingredientList[0].standardQ - (flourA % ingredientList[0].standardQ);
+    int eggC = ingredientList[1].standardQ - (eggA % ingredientList[1].standardQ);
+    int butterC = ingredientList[2].standardQ - (butterA % ingredientList[2].standardQ);
+    int chocoC = ingredientList[3].standardQ - (chocoA % ingredientList[3].standardQ);
+    int milkC = ingredientList[4].standardQ - (milkA % ingredientList[4].standardQ);
+
+    List constraints = [flourC, eggC, butterC, chocoC, milkC];
+
+    List<int> results = recommend(constraints, recipeList);
+
+    Map<Recipe, int> map = Map();
+    for(int i = 0; i < results.length; i++){
+      if(results[i] > 0){
+        map.putIfAbsent(recipeList[i], () => results[i]);
+      }
+    }
+
+    return map;
+
+
+
+}
+
+List<int> recommend(List<int> constraints, List<Recipe>recipeList){
+
+  var tracker = new List.generate(constraints[0] + 1, (_) =>
+  new List.generate(constraints[1] + 1, (_) =>
+  new List.generate(constraints[2] + 1, (_) =>
+  new List.generate(constraints[3] + 1, (_) =>
+  new List.generate(constraints[4] + 1, (_) =>
+  new List.filled(constraints.length, 0.0),
+      growable: false),
+      growable: false),
+      growable: false),
+      growable: false),
+      growable: false);
+
+  print("tracker");
+  print(tracker[0][0][0]);
+  var store = new List.generate(constraints[0] + 1, (_) =>
+  new List.generate(constraints[1] + 1, (_) =>
+  new List.generate(constraints[2] + 1, (_) =>
+  new List.generate(constraints[3] + 1, (_) =>
+  new List.generate(constraints[4] + 1, (_) =>
+  new List.filled(constraints.length, 0.0),
+      growable: false),
+      growable: false),
+      growable: false),
+      growable: false),
+      growable: false);
+
+  print("store");
+  print(store[0][0][0]);
+
+  for(var a = 0 ; a <= constraints[0]; a++){
+    for(var b = 0; b <= constraints[1]; b++){
+      for(var c = 0; c <= constraints[2]; c++){
+        for(var d = 0; d <= constraints[3]; d++){
+          for(var e = 0; e <= constraints[4]; e++){
+            for(var i = 0; i < recipeList.length; i++){
+              var current = recipeList[i];
+
+              if (current.ingredientsQ[0] > a){
+                continue;
+              }
+              if (current.ingredientsQ[1] > b){
+                continue;
+              }
+              if (current.ingredientsQ[2] > c){
+                continue;
+              }
+              if (current.ingredientsQ[3] > d){
+                continue;
+              }
+              if (current.ingredientsQ[4] > e){
+                continue;
+              }
+
+              var toCompare = calcListAvg(tracker[a][b][c][d][e]);
+
+              tracker[a][b][c][d][e]
+              = findMax(tracker[a][b][c][d][e]
+                  , findValue(tracker[a-current.ingredientsQ[0]][b-current.ingredientsQ[1]][c-current.ingredientsQ[2]][d-current.ingredientsQ[3]][e-current.ingredientsQ[4]]
+                      , current, constraints));
+
+              if(toCompare <  calcListAvg(tracker[a][b][c][d][e])){
+
+                store[a][b][c][d][e] = List.from(store[a-current.ingredientsQ[0]][b-current.ingredientsQ[1]][c-current.ingredientsQ[2]][d-current.ingredientsQ[3]][e-current.ingredientsQ[4]]);
+                store[a][b][c][d][e][i] = store[a][b][c][d][e][i] + 1;
+
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  List result;
+  double currentMax = 0.0;
+  for(var a = 0 ; a <= constraints[0]; a++){
+    for(var b = 0; b <= constraints[1]; b++){
+      for(var c = 0; c <= constraints[2]; c++){
+        for(var d = 0; d <= constraints[3]; d++){
+          for(var e = 0; e <= constraints[4]; e++){
+            if(calcListAvg(tracker[a][b][c][d][e]) > currentMax) {
+              currentMax = calcListAvg(tracker[a][b][c][d][e]);
+              result = store[a][b][c][d][e];
+            }
+          }
+        }
+      }
+    }
+  }
+
+  print("result");
+  print(result);
+
+  return result;
 
 
 }
