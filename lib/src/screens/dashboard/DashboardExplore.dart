4bfc22ab2f20cfccbcdb760606123509cabd701a
blankshiro <edwin.tok.2019@sis.smu.edu.sg>
2021-03-24 09:46:36 +0800

    Update DashboardExplore.dart

--- lib/src/screens/dashboard/DashboardExplore.dart
+++ lib/src/screens/dashboard/DashboardExplore.dart
@@ -352,18 +352,106 @@ class _DashboardExploreState extends State<DashboardExplore> {
                     ],
                   ),
                 )),
-            SizedBox(
-              width: 8,
-            ),
-            // Container
+            // SizedBox(
+            //   width: 8,
+            // ),
             Container(
-              height: 192,
-              child: PageView(
-                children: <Widget>[
-                    getChildWidget(),
+              width: 300,
+              height: 300,
+              color: Colors.grey[100],
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 80,
+                    left: 20,
+                    child: Container(
+                      width: 150.0,
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Colors.redAccent[200],
+                            Colors.yellow[50],
+                          ],
+                        ),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(16.0),
+                          topRight: Radius.circular(80.0),
+                          bottomLeft: Radius.circular(16.0),
+                          bottomRight: Radius.circular(16.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 30,
+                    child: Container(
+                      alignment: Alignment.topLeft,
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ClipRect(
+                        clipBehavior: Clip.hardEdge,
+                        child: OverflowBox(
+                          maxHeight: 120,
+                          maxWidth: 120,
+                          alignment: Alignment.topLeft,
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Container(
+                              alignment: Alignment.topLeft,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  alignment: Alignment.topLeft,
+                                  image: AssetImage(
+                                      'assets/images/choc_cupcake.png'),
+                                ),
+                                color: Colors.white.withOpacity(0.4),
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 160,
+                    left: 40,
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Breakfast',
+                        style: h7,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 200,
+                    left: 40,
+                    child: Container(
+                      width: 110,
+                      child: Text(
+                        'Chocolate Cupcake x10',
+                        style: h6,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
+            // Container(
+            //   height: 192,
+            //   child: PageView(
+            //     children: <Widget>[
+            //       getChildWidget(),
+            //     ],
+            //   ),
+            // ),
             SizedBox(
               height: 16,
             ),
